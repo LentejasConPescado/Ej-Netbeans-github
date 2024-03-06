15bc3184b1b13b99aa10980a86a698570f03b01e
Gabriel <gabrielrvlt@gmail.com>
2024-03-06 17:54:10 +0100

    base antes de rectangulo

---
+++ src/proyecto_con_git/Rectangulo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto_con_git;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class Rectangulo
+{
+    float alto = 0.0f;
+    float ancho = 0.0f;
+    public Rectangulo(float l1, float l2)
+    {
+        alto = l1;
+        ancho = l2;
+    }
+}
